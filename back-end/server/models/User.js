9cadebd4df57e18ea63210a6f06161da1ab7fc8c
UnLasToV <som3470100921435@gmail.com>
2024-09-11 05:10:21 +0700

    first commit

---
+++ back-end/server/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+});
+
+// ฟังก์ชัน pre-save สำหรับการเข้ารหัส password ก่อนบันทึก
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// ฟังก์ชันสำหรับเปรียบเทียบรหัสผ่าน
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
